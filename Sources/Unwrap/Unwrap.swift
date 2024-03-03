ab8853417150912d6ac9135a6fddb8f76d4ca2e6
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2024-03-03 17:06:30 +0330

    docs: add a simple example

--- Sources/Unwrap/Unwrap.swift
+++ Sources/Unwrap/Unwrap.swift
@@ -1,4 +1,9 @@
 /// A macro that takes an `Optional` and returns the wrapped value or throws an error if found `nil`.
+/// For example,
+///
+///     try #unwrap(myOptional)
+///
+/// tries unwrapping the `myOptional` if it has a value. Otherwise, it throws an error to catch.
 @freestanding(expression)
 public macro unwrap<Wrapped>(_ value: Wrapped?) -> Wrapped = #externalMacro(
     module: "UnwrapMacros",
