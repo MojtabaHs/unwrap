590dfb6c250e89d8e39151311beedf638aa12f1c
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2024-03-03 16:44:50 +0330

    feat: make the Optional.Error, CustomStringConvertible

--- Sources/Unwrap/Optional.Error.swift
+++ Sources/Unwrap/Optional.Error.swift
@@ -1,5 +1,11 @@
 public extension Optional {
-    enum Error: Swift.Error {
+    enum Error: Swift.Error, CustomStringConvertible {
         case unexpectedNil(_ optional: String)
+
+        public var description: String {
+            switch self {
+            case let .unexpectedNil(optional): #"Unexpectedly found nil while unwrapping "\#(optional)""#
+            }
+        }
     }
 }
